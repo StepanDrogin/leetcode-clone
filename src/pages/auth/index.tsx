b3e8cf6a4174161d74a7e50047580436bdddad1b
Stepan <s.drogin@think24.ru>
2023-07-27 07:39:08 +0400

    added auth page route

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div>Here is auth page</div>
+}
+export default AuthPage;
