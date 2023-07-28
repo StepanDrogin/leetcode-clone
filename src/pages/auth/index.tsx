f11ecd73280beab46dcbe425fa6b06fec71db8e0
Stepan <s.drogin@think24.ru>
2023-07-28 15:26:58 +0400

    added authmodal and navbar

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,3 +1,5 @@
+import AuthModal from '@/components/Modals/AuthModal';
+import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
 
 type AuthPageProps = {
@@ -6,6 +8,14 @@ type AuthPageProps = {
 
 const AuthPage:React.FC<AuthPageProps> = () => {
     
-    return <div>Here is auth page</div>
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+            <Navbar/>
+            <div className='flex items-center justify-center h-[calc(100vh-5rem)]'>
+                <img src="/hero.png" alt="Hero image" />
+                <AuthModal></AuthModal>
+            </div>
+        </div>
+    </div>
 }
 export default AuthPage;
