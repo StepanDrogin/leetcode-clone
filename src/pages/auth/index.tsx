d599373758af163fb3d48c4b5e1d927519c7ccce
Stepan <s.drogin@think24.ru>
2023-07-29 09:44:59 +0400

    imported authModal

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,19 +1,21 @@
+import { authModalState } from '@/atoms/authModalAtom';
 import AuthModal from '@/components/Modals/AuthModal';
 import Navbar from '@/components/Navbar/Navbar';
 import React from 'react';
+import { useRecoilValue } from 'recoil';
 
 type AuthPageProps = {
     
 };
 
 const AuthPage:React.FC<AuthPageProps> = () => {
-    
+    const authModal = useRecoilValue(authModalState)
     return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
         <div className='max-w-7xl mx-auto'>
             <Navbar/>
             <div className='flex items-center justify-center h-[calc(100vh-5rem)]'>
                 <img src="/hero.png" alt="Hero image" />
-                <AuthModal></AuthModal>
+                {authModal.isOpen && <AuthModal/>}
             </div>
         </div>
     </div>
