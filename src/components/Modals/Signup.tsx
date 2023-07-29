c2d79d4f264cc2a6c88d7da5df505c6390857890
Stepan <s.drogin@think24.ru>
2023-07-29 09:44:29 +0400

    added closemodal and functions on buttons

--- src/components/Modals/Signup.tsx
+++ src/components/Modals/Signup.tsx
@@ -1,11 +1,15 @@
+import { authModalState } from '@/atoms/authModalAtom';
 import React from 'react';
+import { useSetRecoilState } from "recoil";
 
 type SignupProps = {
     
 };
 
 const Signup:React.FC<SignupProps> = () => {
-    
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const handleClick = () => {
+		setAuthModalState((prev) => ({ ...prev, type: "login" }))};
     return (
         <form className='space-y-6 px-6 pb-4'>
         <h3 className='text-xl font-medium text-white'>Register to LeetClone</h3>
@@ -64,7 +68,7 @@ const Signup:React.FC<SignupProps> = () => {
         </button>
         <div className='text-sm font-medium text-gray-300'>
             Already have an account?{" "}
-            <a href='#' className='text-blue-700 hover:underline'>
+            <a href='#' className='text-blue-700 hover:underline' onClick={handleClick}>
                 Log in
             </a>
         </div>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
 import Link from 'next/link';
 import React from 'react';
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "@/atoms/authModalAtom";
 
 type NavbarProps = {
     
 };
 
 const Navbar:React.FC<NavbarProps> = () => {
-    
+	const setAuthModalState = useSetRecoilState(authModalState);
+	const handleClick = () => {
+		setAuthModalState((prev) => ({ ...prev, isOpen: true }));
+	};
     return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24'>
-        <Link href="/" className='flex items-center justify-center h-20'>
+        <Link href="" className='flex items-center justify-center h-20'>
             <img src='/logo.png' alt='LeetClone' className='h-full'>
             </img>
             <div className="flex items-center">
@@ -17,6 +22,7 @@ const Navbar:React.FC<NavbarProps> = () => {
                 hover:text-brand-orange hover:bg-white hover:border-2 hover:border-brand-orange border-2 border-transparent
                 transition duration-300 ease-in-out
                 '
+                onClick={handleClick}
                 >Sign In</button>
             </div>
         </Link>
