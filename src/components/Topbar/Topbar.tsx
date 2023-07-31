3f28598cb47c6d75a63845f826cb4f2ab854f19a
Stepan <s.drogin@think24.ru>
2023-07-31 14:21:07 +0400

    Finished with auth funtionality

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,12 +1,18 @@
+import { auth } from '@/firebase/firebase';
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import Logout from '../Buttons/Logout';
+import { useSetRecoilState } from 'recoil';
+import { authModalState } from '@/atoms/authModalAtom';
 
 type TopbarProps = {
     
 };
 
 const Topbar:React.FC<TopbarProps> = () => {
-    
+    const [user] = useAuthState(auth);
+	const setAuthModalState = useSetRecoilState(authModalState);
     return (
 		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
 			<div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
@@ -24,19 +30,29 @@ const Topbar:React.FC<TopbarProps> = () => {
 							Premium
 						</a>
 					</div>
+						{!user && (
 						<Link
 							href='/auth'
+							onClick={() => {
+								setAuthModalState((prev) => ({...prev, isOpen: true, type: 'login'}))
+							}}
 						>
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 						</Link>
-						<div className='cursor-pointer group relative'>
-							<div
+							)}
+							{user && (
+								<div className='cursor-pointer group relative'>
+									<img src='/avatar.png' alt='user profile image' className='h-8 w-8 rounded-full'></img>
+									<div
 								className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
 								z-40 group-hover:scale-100 scale-0 
 								transition-all duration-300 ease-in-out'
 							>
+								<p className='text-sm'>{user.email}</p>
 							</div>
 						</div>
+							)}
+							{user && <Logout></Logout>}
 				</div>
 			</div>
 		</nav>
