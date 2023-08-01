55ea21f13cc5c4b9b83cae220618022d8029b4f9
Stepan <s.drogin@think24.ru>
2023-08-01 12:41:51 +0400

    created problems route component

---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,14 @@
+import Topbar from '@/components/Topbar/Topbar';
+import Workspace from '@/components/Workspace/Workspace';
+import React from 'react';
+
+type ProblemPageProps = {};
+
+const ProblemPage:React.FC<ProblemPageProps> = () => {
+    
+    return <div>
+        <Topbar problemPage></Topbar>
+        <Workspace></Workspace>
+    </div>
+}
+export default ProblemPage;
