3e6f9a2777ff31232b0c297f46079496532acaab
Stepan <s.drogin@think24.ru>
2023-08-02 11:29:41 +0400

    added editorfooter

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -4,21 +4,24 @@ import Split from 'react-split';
 import CodeMirror from '@uiw/react-codemirror';
 import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 import { javascript } from '@codemirror/lang-javascript';
+import EditorFooter from './EditorFooter/EditorFooter';
 
 type PlaygroundProps = {
     
 };
 
 const Playground:React.FC<PlaygroundProps> = () => {
-    
+    const boilerPlate = `function twoSum(nums, target) {
+        // Write your code here
+    };`;
     return (
-        <div className='flex flex-col bg-dark-layer-1 relative'>
+        <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
         <PreferenceNav></PreferenceNav>
 
         <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60,40]} minSize={60}>
             <div className='w-full overflow-auto'>
                 <CodeMirror
-                value='const a = 1;'
+                value={boilerPlate}
                 theme={vscodeDark}
                 extensions={[javascript()]}
                 style={{fontSize:16}}
@@ -29,7 +32,7 @@ const Playground:React.FC<PlaygroundProps> = () => {
                 <div className='flex h-10 items-center space-x-6'>
                     <div className='relative flex h-full flex-col justify-center cursor-pointer'>
                         <div className='text-small font-medium leading-5 text-white'>Test cases</div>
-                        <hr className='absolute bottom-0 h-0.5 w-20 rounded-full border-none bg-white' />
+                        <hr className='absolute bottom-0 w-full h-0.5 rounded-full border-none bg-white' />
                     </div>
                 </div>
                 <div className='flex'>
@@ -58,8 +61,19 @@ const Playground:React.FC<PlaygroundProps> = () => {
                         </div>
                     </div>
                 </div>
+                <div className='font-semibold my-4'>
+                    <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                    <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                        nums: [2,7,11,15], target: 9
+                    </div>
+                    <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                    <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                        [0,1]
+                    </div>
+                </div>
             </div>
         </Split>
+        <EditorFooter></EditorFooter>
         </div>
     );
 };
