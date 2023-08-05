7d37b8677e24a2e7281137aa3060994a535e4cea
Stepan <s.drogin@think24.ru>
2023-08-05 17:12:24 +0400

    Fixing merge conflict

--- src/utils/types/problem.ts
+++ src/utils/types/problem.ts
@@ -17,7 +17,6 @@ export type Problem = {
 	starterCode: string;
 	handlerFunction: ((fn: any) => boolean) | string;
 	starterFunctionName: string;
-<<<<<<< HEAD
 };
 
 export type DBProblem = {
@@ -30,6 +29,4 @@ export type DBProblem = {
 	order: number;
 	videoId?: string;
 	link?: string;
-=======
->>>>>>> 6be5783e7e50cfa11b448d1be552c62d661a6b9e
 };
